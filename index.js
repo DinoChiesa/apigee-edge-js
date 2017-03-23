1fe124d0e8dd7a23943f66d0e7a7d6770d98e2f2
Dino Chiesa <dpchiesa@hotmail.com>
2017-03-23 14:37:19 -0700

    create index.js to include the other modules

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+// ------------------------------------------------------------------
+//
+// main entry point for apigee-edge-js
+//
+// created: Thu Mar 23 14:34:12 2017
+// last saved: <2017-March-23 14:35:14>
+
+var edge = require('./lib/edge.js'),
+    utility = require('./lib/utility.js');
+
+module.exports = {
+  edge : edge,
+  utility : utility
+};
