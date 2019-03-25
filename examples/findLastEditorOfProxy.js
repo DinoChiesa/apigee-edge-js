5cf4ff50112f9be871cafef00a120405026d4faa
Dino Chiesa <dchiesa@google.com>
2019-03-25 11:03:09 -0700

    update to use promises

--- examples/findLastEditorOfProxy.js
+++ examples/findLastEditorOfProxy.js
@@ -15,6 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+/* global process */
 
 const request            = require('request'),
       edgejs             = require('apigee-edge-js'),
@@ -22,10 +23,18 @@ const request            = require('request'),
       apigeeEdge         = edgejs.edge,
       sprintf            = require('sprintf-js').sprintf,
       eightHoursInMillis = 8 * 60 * 60 * 1000,
-      version            = '20190211-1311',
+      lookbackInterval   = eightHoursInMillis,
+      version            = '20190325-1056',
+      regexi             = {
+        CREATE : [
+          new RegExp('/v1/(?:o|organizations)/[^/]+/apis/([^/]+)/\\?action=import&name=[\\S]+$'),
+          new RegExp('/v1/(?:o|organizations)/[^/]+/apis/([^/]+)/\\?(?:[^&]+&)action=import$')
+        ],
+        UPDATE : [ new RegExp('/v1/(?:o|organizations)/[^/]+/apis/([^/]+)/(?:r|revisions)/[0-9]+.*') ]
+      },
       Getopt             = require('node-getopt'),
       getopt             = new Getopt(common.commonOptions.concat([
-        ['P' , 'proxy=ARG+', 'the proxy to find. You can pecify this option multiple times.']
+        ['P' , 'proxy=ARG+', 'Required. the proxy to find. You can specify this option multiple times.']
       ])).bindHelp();
 
 function joinUrlElements() {
@@ -41,82 +50,79 @@ function transformArrayIntoHash(a) {
     }, {});
 }
 
-
 // ========================================================
 
-function proxiesOfInterest(proxyData) {
-  return Object.keys(proxyData).filter(function(key) {
-    return(proxyData[key] == null);
-  });
+function proxiesRemaining(proxyData) {
+  return Object.keys(proxyData).filter(key => proxyData[key] == null);
 }
 
 function processOneBatch(records, proxyData) {
-  var regexi = {
-        CREATE : new RegExp('/v1/(?:o|organizations)/[^/]+/apis/([^/]+)/\\?action=import&name=[\\S]+$'),
-        UPDATE : new RegExp('/v1/(?:o|organizations)/[^/]+/apis/([^/]+)/(?:r|revisions)/[0-9]+.*')
-      };
-  var ofInterest = proxiesOfInterest(proxyData);
-  ofInterest.forEach(function(proxyName){
-    records.forEach(function(record) {
-      var a;
-      //console.log('looking at: %s %s', record.operation, record.requestUri);
-      Object.keys(regexi).forEach(function(key) {
-        var regex = regexi[key];
-        if (record.operation == key) {
+  const notYetFound = proxiesRemaining(proxyData);
+  records.forEach(function(record) {
+    // if (opt.options.verbose) {
+    //   console.log('looking at: %s %s', record.operation, record.requestUri);
+    // }
+    Object.keys(regexi).forEach(function(key) {
+      if (record.operation == key) {
+        regexi[key].forEach(regex => {
           var a = regex.exec(record.requestUri);
           if (a != null) {
-            //console.log('found %s', key);
-            if ((a[1] == proxyName) && proxyData[proxyName] == null) {
-              //console.log('matched %s %s', key, proxyName);
-              proxyData[proxyName] = record;
-              proxyData[proxyName].humanTime = new Date(record.timeStamp).toISOString();
-            }
+            notYetFound.forEach(function(proxyName){
+              if ((proxyData[proxyName] == null) && (a[1] == proxyName)) {
+                //console.log('matched %s %s', key, proxyName);
+                proxyData[proxyName] = record;
+                proxyData[proxyName].humanTime = new Date(record.timeStamp).toISOString();
+              }
+            });
           }
-        }
-      });
+        });
+      }
     });
   });
 }
 
-function moreToDo(proxyData) {
-  return (proxiesOfInterest(proxyData).length == 0);
+function findAuditRecords(org, proxyData) {
+  const now = (new Date()).getTime();
+  const endTime = now;
+  const startTime = now - lookbackInterval;
+
+  return getOneBatch(org, startTime, endTime, transformArrayIntoHash(proxyData));
 }
 
-function getOneBatch(org, startTime, endTime, proxyData, cb) {
-  if ( ! proxyData) {
-    return cb(new Error("missing proxyData"));
 
-  }
-  if (moreToDo(proxyData)) {
-    return cb(null, proxyData);
-  }
+function getOneBatch(org, startTime, endTime, proxyData) {
+  return new Promise( (resolve, reject) => {
+    if ( ! proxyData) {
+      return reject(new Error("missing proxyData"));
+    }
 
-  var auditQuery = sprintf("expand=true&endTime=%d&startTime=%d", endTime, startTime);
-  org.audits.get({endTime:endTime, startTime:startTime},
-                 function (error, response) {
-                   var result;
-                   if (error) {
-                     console.log(error);
-                     return cb(error);
-                   }
-                   //console.log(JSON.stringify(response));
-                   if (response.auditRecord) {
-                     //console.log('%d records', records.length);
-                     if (!opt.options.verbose) {
-                       process.stdout.write('.');
-                     }
-                     if (response.auditRecord.length > 0) {
-                       processOneBatch(response.auditRecord, proxyData);
-                     }
-                     // invoke again
-                     endTime = startTime + (10 * 1000); // fudge
-                     startTime -= eightHoursInMillis;
-                     setTimeout(function() { getOneBatch(org, startTime, endTime, proxyData, cb); }, 1);
-                   }
-                   else {
-                     cb(null, proxyData);
-                   }
-                 });
+    var auditQuery = sprintf("expand=true&endTime=%d&startTime=%d", endTime, startTime);
+    org.audits.get({startTime, endTime})
+      .then( response => {
+        var result;
+        //console.log(JSON.stringify(response));
+        if (response.auditRecord) {
+          //console.log('%d records', response.auditRecord.length);
+          if (!opt.options.verbose) {
+            process.stdout.write('.');
+          }
+          if (response.auditRecord.length > 0) {
+            processOneBatch(response.auditRecord, proxyData);
+          }
+          if ((proxiesRemaining(proxyData).length == 0)) {
+            return resolve(proxyData);
+          }
+          // invoke again
+          endTime = startTime + (10 * 1000); // fudge 10s
+          startTime -= lookbackInterval;     // backup one interval
+          setTimeout(function() { getOneBatch(org, startTime, endTime, proxyData); }, 1);
+        }
+        else {
+          return resolve(proxyData);
+        }
+      })
+      .catch( e => reject(e) );
+  });
 }
 
 
@@ -138,29 +144,12 @@ if ( !opt.options.proxy ) {
 }
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
-apigeeEdge.connect(common.optToOptions(opt), function(e, org) {
-  if (e) {
-    common.logWrite(JSON.stringify(e, null, 2));
-    //console.log(e.stack);
-    process.exit(1);
-  }
-  common.logWrite('connected');
 
-  //const auditUrlBase = joinUrlElements(opt.options.mgmtserver, auditBase, opt.options.org);
-  const now = (new Date()).getTime();
-  const endTime = now;
-  const startTime = now - eightHoursInMillis;
-
-  const proxyData = transformArrayIntoHash(opt.options.proxy);
-
-  getOneBatch(org, startTime, endTime, proxyData, function(e, results) {
-    console.log('\n');
-    if (e) {
-      console.log(e);
-    }
-    else {
-      console.log(JSON.stringify(proxyData, null, 2));
-    }
-  });
-
-});
+apigeeEdge.connect(common.optToOptions(opt))
+  .then(org =>
+    findAuditRecords(org, opt.options.proxy)
+      .then( results => {
+        console.log('\n');
+        console.log(JSON.stringify(results, null, 2));
+      }))
+  .catch( e => console.log('error: ' + e.stack));
