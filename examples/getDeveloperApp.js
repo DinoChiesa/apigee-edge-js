b29a1efed44ee84904993676a99552346660dd45
Dino Chiesa <dchiesa@google.com>
2019-09-25 17:21:44 -0700

    promisify getDeveloperApp

--- examples/getDeveloperApp.js
+++ examples/getDeveloperApp.js
@@ -2,29 +2,20 @@
 // ------------------------------------------------------------------
 //
 // created: Sat Mar 17 17:20:25 2018
-// last saved: <2019-February-11 13:07:42>
-/* jshint esversion: 6, node: true */
+// last saved: <2019-September-25 17:16:34>
+/* jshint esversion: 9, node: true, strict:implied */
 /* global process, console */
-
-'use strict';
 
 const edgejs     = require('apigee-edge-js'),
       common     = edgejs.utility,
       apigeeEdge = edgejs.edge,
       Getopt     = require('node-getopt'),
-      version    = '20190211-1306',
+      util       = require('util'),
+      version    = '20190925-1712',
       getopt     = new Getopt(common.commonOptions.concat([
-        ['A' , 'app=ARG', 'Required. the name of the app to query.'],
+        ['A' , 'app=ARG', 'Optional. the name of the app to query. Without this, lists apps.'],
         ['D' , 'developer=ARG', 'Required. the developer that owns the app.']
       ])).bindHelp();
-
-function handleError(e) {
-    if (e) {
-      console.log(e);
-      console.log(e.stack);
-      process.exit(1);
-    }
-}
 
 // ========================================================
 
@@ -39,23 +30,16 @@ var opt = getopt.parse(process.argv.slice(2));
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
-if ( !opt.options.app ) {
-  console.log('You must specify an App to query');
-  getopt.showHelp();
-  process.exit(1);
-}
-
 if ( !opt.options.developer ) {
   console.log('You must specify a developer');
   getopt.showHelp();
   process.exit(1);
 }
 
-apigeeEdge.connect(common.optToOptions(opt), function(e, org) {
-  handleError(e);
-  common.logWrite('searching...');
-  org.developerapps.get({name:opt.options.app, email:opt.options.developer}, function(e, app) {
-    handleError(e);
-    console.log('app: ' + JSON.stringify(app, null, 2));
-  });
-});
+apigeeEdge.connect(common.optToOptions(opt))
+  .then( org => {
+    common.logWrite('searching...');
+    return org.developerapps.get({name:opt.options.app, email:opt.options.developer})
+      .then ( app => console.log(JSON.stringify(app, null, 2)) );
+  })
+  .catch( e => console.error('error: ' + util.format(e) ) );
