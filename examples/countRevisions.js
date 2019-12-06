4bf9dcfcda527e5b8ca140ac1cf7631990659c1d
Dino Chiesa <dchiesa@google.com>
2019-12-05 19:47:11 -0800

    accept regex in countRevisions.js

--- examples/countRevisions.js
+++ examples/countRevisions.js
@@ -1,8 +1,9 @@
+#! /usr/local/bin/node
 // countRevisions.js
 // ------------------------------------------------------------------
 //
 // created: Mon Dec  3 13:31:48 2018
-// last saved: <2019-September-25 17:41:18>
+// last saved: <2019-December-05 19:47:05>
 
 /* jshint esversion: 9, node: true, strict:implied */
 /* global process, console, Buffer */
@@ -12,11 +13,32 @@ const edgejs     = require('apigee-edge-js'),
       apigeeEdge = edgejs.edge,
       Getopt     = require('node-getopt'),
       util       = require('util'),
-      version    = '20190925-1612',
+      version    = '20191205-1947',
       getopt     = new Getopt(common.commonOptions.concat([
         ['P' , 'prefix=ARG', 'optional. name prefix. query revision of proxies with names starting with this prefix.' ],
+        ['R' , 'regex=ARG', 'optional. a regular expression. query revision of proxies with names matching this pattern.' ],
         ['S' , 'sharedflow', 'optional. query sharedflows. Default: query proxies.']
       ])).bindHelp();
+
+
+function isKeeper(opt) {
+  if (opt.options.regex) {
+    common.logWrite('using regex match (%s)',opt.options.regex);
+    let re1 = new RegExp(opt.options.regex);
+    return function(name) {
+      return name.match(re1);
+    };
+  }
+
+  if (opt.options.prefix) {
+    common.logWrite('using prefix match (%s)',opt.options.prefix);
+    return function (name) {
+      return name.startsWith(opt.options.prefix);
+    };
+  }
+
+  return () => true;
+}
 
 // ========================================================
 
@@ -38,7 +60,7 @@ apigeeEdge.connect(common.optToOptions(opt))
       .then( items => {
         items = items
           .sort()
-          .filter( name => (! opt.options.prefix) || name.startsWith(opt.options.prefix ));
+          .filter( isKeeper(opt) );
 
         if ( !items || items.length == 0) {
           return Promise.resolve(true);
